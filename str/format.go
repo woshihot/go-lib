a50e49117bafb4cf1e02976545e853227bc431b1
zhaohongjian <woshihotZHJ831>
2019-12-10 16:05:44 +0800

    time

--- str/format.go
+++ str/format.go
@@ -1,12 +1,10 @@
 package str
 
 import (
-	"github.com/woshihot/go-lib/log"
 	"time"
 )
 
 func TimeFormat(t time.Time) string {
 	r := t.Format("2006-01-02 15:04:05.999")
-	log.Df("TimeFormat time = %s\n", r)
 	return r
 }
