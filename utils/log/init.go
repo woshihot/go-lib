ff02c7b984e6f8bb5c511162639179273e42d82c
zhaohongjian <woshihotZHJ831>
2019-12-10 17:50:11 +0800

    log+

--- utils/log/init.go
+++ utils/log/init.go
@@ -2,16 +2,18 @@ package log
 
 import (
 	"io"
-	"log"
 	"os"
 )
 
 var LogOutput io.Writer
 
-func Init(sysName string, writers ...io.Writer) {
+// set sysName-prefix for log
+// set flag for log
+// set multiWriter for log
+func Init(sysName string, flags int, writers ...io.Writer) {
 	writers = append(writers, os.Stdout)
 	LogOutput = io.MultiWriter(writers...)
-	log.SetPrefix("[" + sysName + "] ")
-	log.SetOutput(LogOutput)
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	SetPrefix("[" + sysName + "] ")
+	SetOutput(LogOutput)
+	SetFlags(flags)
 }
